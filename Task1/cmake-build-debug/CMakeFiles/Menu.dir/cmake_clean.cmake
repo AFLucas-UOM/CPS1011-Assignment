86337a34f09fd1d7a1ba462e0a64a624bfcf8e6b
Andrea Filiberto Lucas <andrea.f.lucas.22@um.edu.mt>
2023-01-13 13:22:05 +0100

    Cmake_update_folder Day2

---
+++ Task1/cmake-build-debug/CMakeFiles/Menu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Menu.dir/PROJ_task1q3_menu.c.o"
+  "CMakeFiles/Menu.dir/PROJ_task1q3_menu.c.o.d"
+  "Menu"
+  "Menu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Menu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
