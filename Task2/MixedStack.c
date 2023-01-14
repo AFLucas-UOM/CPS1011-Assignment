786ec0110a6265712e72e554a95ee12c5a887bd8
Andrea Filiberto Lucas <andrea.f.lucas.22@um.edu.mt>
2023-01-14 13:42:43 +0100

    MixedStack (EXPORT ISSUE) Day4

--- Task2/MixedStack.c
+++ Task2/MixedStack.c
@@ -1,12 +1,97 @@
 #include <stdio.h>
-#include "MixedStack_t.h"
-//#include "MixedStack_t(b).h"
+#include "MixedStack_t.h" // Link to MixedStack_t.h
 
-int main(void) {
-    MixedStack_elm stack;
+//decoder
+void int_decoder(void* value, char* buffer) {
+    sprintf(buffer, "%d", *(int*)value); // convert int value to string and store it in the buffer
+}
+void string_decoder(void* value, char* buffer) {
+    strncpy(buffer, (char*)value, 64); // copy the string value to the buffer
+}
+
+// Main() Function
+int main() {
+    // Create a new mixed stack
+    MixedStack stack;
     initMixedStack(&stack);
-    //mixedstack_push(&stack, 3);
-    push(&stack, "hello");
-    // ...
+
+    //pre-defined Testing Values --> can be user input
+    // Test push() function for int
+    int value1 = 101;
+    push(&stack, INTEGER_TYPE, &value1);
+    int value2 = 26;
+    push(&stack, INTEGER_TYPE, &value2);
+    int value3 = 21;
+    push(&stack, INTEGER_TYPE, &value3);
+    int value4 = 2023;
+    push(&stack, INTEGER_TYPE, &value4);
+    // Test push() function for String
+    char value5[] = "my string test";
+    push(&stack, STRING_TYPE, value5);
+
+    // Test export() function NOT WORKING ATM
+    const char filename[] = "exported_stack.txt";
+    if (stack.top->type == INTEGER_TYPE) {
+        export(&stack, int_decoder, filename);
+    } else if(stack.top->type == STRING_TYPE) {
+        export(&stack, string_decoder, filename);
+    }
+
+    // Test isEmpty() function
+    char* is_empty = isEmpty(&stack);
+    printf("Is stack empty? %s\n", is_empty);
+
+    // Check if the stack is full
+    if (isFull(&stack)) {
+        printf("Stack is currently full\n");
+    } else {
+        printf("Stack is not currently full\n");
+    }
+
+    // Test count() function
+    int stack_size = count(&stack);
+    printf("Current Stack size: %d\n", stack_size);
+
+    // Test peek() function
+    MixedStack_elm* top_element = peek(&stack);
+    if (top_element->type == INTEGER_TYPE) {
+        printf("Top element in Stack is: %d\n", top_element->value.int_value);
+    } else if (top_element->type == STRING_TYPE) {
+        printf("Top element in Stack is: '%s'\n", top_element->value.string_value);
+    }
+
+    // Test pop() function
+    MixedStack_elm* popped_element = pop(&stack);
+    if (top_element->type == INTEGER_TYPE) {
+        printf("\nPopped element: %d\n", popped_element->value.int_value);
+    } else if (top_element->type == STRING_TYPE) {
+        printf("\nPopped element: %s\n", popped_element->value.string_value);
+    }
+
+    // Test peek() function
+    MixedStack_elm* top_element2 = peek(&stack);
+    if (top_element2->type == INTEGER_TYPE) {
+        printf("Top element: %d\n", top_element2->value.int_value);
+    } else if (top_element2->type == STRING_TYPE) {
+        printf("Top element: %s\n", top_element2->value.string_value);
+    }else
+
+    // Test clear() function
+        clear(&stack);
+    // Check if the stack is empty post Clear function
+    if (isEmpty(&stack)) {
+        printf("The stack is NOW empty\n");
+    } else {
+        printf("The stack is still not empty\n");
+    }
+
+    // Test deinitMixedStack() function
+    deinitMixedStack(&stack);
+    // Test isEmpty() function again post deinitMixedStack() function
+    if (isEmpty(&stack)) {
+        printf("The stack is NOW empty\n");
+    } else {
+        printf("The stack is still not empty\n");
+    }
     return 0;
 }
--- Task2/MixedStack_t.h
+++ Task2/MixedStack_t.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> // used for strncpy()
 
 #define INTEGER_TYPE 0
 #define STRING_TYPE 1
@@ -24,97 +25,144 @@ typedef struct {
     } value;
 } MixedStack;
 
-
-// Create a new mixed stack and allocate memory resources
+// Create a new mixed stack and allocate memory resources [initMixedStack]
 void initMixedStack(MixedStack* stack) {
-    stack->top = NULL;
-    stack->size = 0;
-}
-
-// Destroy an existing mixed stack and release memory resources
-void deinit_MixedStack(MixedStack* stack) {
-    MixedStack_elm* node = stack->top;
-    while (node != NULL) {
-        MixedStack_elm* temp = node;
-        node = node->next;
-        free(temp);
-    }
-    stack->top = NULL;
-    stack->size = 0;
-}
-
-// Add an element of some type on top of the mixed stack
-void push(MixedStack* stack, int type, void* value) {
-    if(stack->size == STACK_SIZE_LIMIT) {
-        printf("Stack Overflow ERROR!");
+    // Allocate memory for the new stack
+    stack->top = (MixedStack_elm*) malloc(sizeof(MixedStack_elm));
+    if(stack->top == NULL)
+    {
+        printf("Error: Failed to allocate memory for the stack!!");
         return;
     }
-    MixedStack_elm* new_node = (MixedStack_elm*)malloc(sizeof(MixedStack_elm));
-    new_node->type = type;
-    if (type == INTEGER_TYPE) {
-        new_node->value.int_value = *(int*)value;
-    } else {
-        snprintf(new_node->value.string_value, 64, "%s", (char*)value);
+    // Initialize the stack properties
+    stack->top = NULL;
+    stack->size = 0;
+}
+
+// Destroy an existing mixed stack and release memory resources [deinitMixedStack]
+void deinitMixedStack(MixedStack* stack) {
+    if(stack->top == NULL)
+    {
+        printf("Error: The stack is already empty!!");
+        return;
     }
-    new_node->next = stack->top;
-    stack->top = new_node;
+    // Traverse the stack and free each element
+    MixedStack_elm* current = stack->top;
+    while (current != NULL) {
+        MixedStack_elm* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    // Reset the stack properties
+    stack->top = NULL;
+    stack->size = 0;
+}
+
+// Add an element of some type on top of the mixed stack [push]
+void push(MixedStack* stack, int type, void* value) {
+    if (stack->size == STACK_SIZE_LIMIT) {
+        printf("Error: Stack overflow!!");
+        return;
+    }
+    // Allocate memory for the new element
+    MixedStack_elm* new_element = (MixedStack_elm*)malloc(sizeof(MixedStack_elm));
+
+    // Check for NULL pointer
+    if(new_element == NULL)
+    {
+        printf("Error: Failed to allocate memory for the new element!!");
+        return;
+    }
+    // Set the element properties STRING_TYPE 1
+    new_element->type = type;
+    if (type == INTEGER_TYPE) {
+        new_element->value.int_value = *(int*)value;
+    } else if(type == STRING_TYPE) {
+        strncpy(new_element->value.string_value, (char*)value, 64); // copy the string value to the stack element
+    }
+    new_element->next = stack->top;
+
+    // Add the new element to the top of the stack
+    stack->top = new_element;
     stack->size++;
 }
 
-// Remove and return the element on top of the mixed stack
+// Remove and return the element on top of the mixed stack [pop]
 MixedStack_elm* pop(MixedStack* stack) {
     if (stack->top == NULL) {
-        printf("Stack Underflow!");
+        printf("Error: Stack underflow!!");
         return NULL;
     }
-    MixedStack_elm* top = stack->top;
-    stack->top = top->next;
+    // Get the element on top of the stack
+    MixedStack_elm* top_element = stack->top;
+
+    // Update the top pointer and stack size
+    stack->top = top_element->next;
     stack->size--;
-    return top;
+
+    return top_element;
 }
 
-// Returns the top element on the stack without removing it
+// Returns the top element on the stack without removing it [peek]
 MixedStack_elm* peek(MixedStack* stack) {
     if (stack->top == NULL) {
-        printf("Stack is empty!");
+        printf("Error: Stack is empty!!");
         return NULL;
     }
-    return stack->top;
+    // Get the element on top of the stack
+    MixedStack_elm* top_element = stack->top;
+    return top_element;
 }
 
-// Removes all elements from the stack
+// Removes all elements from the stack [clear]
 void clear(MixedStack* stack) {
     while (stack->top != NULL) {
-        pop(stack);
+        // Pop all elements from the stack
+        MixedStack_elm* element = pop(stack);
+        free(element);
     }
 }
 
-// Returns the number of elements currently stored in the stack
+// Returns the number of elements currently stored in the stack [count]
 int count(MixedStack* stack) {
     return stack->size;
 }
 
-// Returns true if the stack is empty, false otherwise
-int isEmpty(MixedStack* stack) {
-    return stack->size == 0;
+// Returns "yes"" if the stack is empty, "no" otherwise [isEmpty]
+char* isEmpty(MixedStack* stack) {
+    if (stack->top == NULL) {
+        return "Yes!!";
+    } else {
+        return "NO!!";
+    }
 }
 
-// Returns true if no more memory resources can be allocated for the stack, false otherwise
+
+// Returns true if no more memory resources can be allocated for the stack, false otherwise [isFull]
 int isFull(MixedStack* stack) {
     return stack->size == STACK_SIZE_LIMIT;
 }
 
-// export the stack to a text file
-void export(MixedStack* stack, void (decoder)(void, char*), char* filename) {
-    filename = "Documents/Uni/Lectures/SEM1/Programming in C/Project_C/TXT/exported_stack.txt";
+// export the stack to a text file [export] [TASK2QB]
+void export(MixedStack* stack, void (*decoder)(void*, char*), char* filename) {
     FILE* file = fopen(filename, "w");
-    MixedStackNode* current = stack->top;
+    if (file == NULL) {
+        printf("Error: Failed to open file %s for writing!!", filename);
+        return;
+    }
+    MixedStack_elm* current = stack->top;
     while (current != NULL) {
-        char* text = (char*)malloc(sizeof(char) * 64);
-        decoder(current->val, text);
+        // Decode the element value to text
+        char text[64];
+        if (current->type == INTEGER_TYPE) {
+            decoder(&current->value.int_value, text);
+        } else {
+            decoder(current->value.string_value, text);
+        }
+        // Write the text to the file
         fprintf(file, "%s\n", text);
+        // Move to the next element
         current = current->next;
-        free(text);
     }
     fclose(file);
 }
